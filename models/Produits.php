764f228026a4d93868db6257f7c9bbe1de8c2396
Julie J <jeannet.julie@gmail.com>
2021-08-23 10:46:35 +0200

    fichiers nécessaires pour la consommation de l'API côté client (testes Postaman ici)

--- models/Produits.php
+++ models/Produits.php
@@ -29,11 +29,11 @@ class Produits{
      */
     public function lire(){
         $sql = "SELECT c.nom as categories_nom, p.id, p.nom, p.description, p.prix, p.categories_id, p.created_at FROM " . $this->table . " p LEFT JOIN categories c ON p.categories_id = c.id ORDER BY p.created_at DESC";
-
+    
         $query = $this->connexion->prepare($sql);
 
         $query->execute();
-
+    
         return $query;
     }
 
@@ -42,24 +42,22 @@ class Produits{
      *
      * @return void
      */
-    function creer(){
-
-        $sql = "INSERT INTO " . $this->table . " SET nom=:nom, prix=:prix, description=:description, categories_id=:categories_id, created_at=:created_at";
-
-        $query = $this->conn->prepare($sql);
-
-        $this->name=htmlspecialchars(strip_tags($this->nom));
+    public function creer(){
+    
+        $sql = "INSERT INTO " . $this->table . " SET nom=:nom, prix=:prix, description=:description, categories_id=:categories_id";
+    
+        $query = $this->connexion->prepare($sql);
+    
+        $this->nom=htmlspecialchars(strip_tags($this->nom));
         $this->prix=htmlspecialchars(strip_tags($this->prix));
         $this->description=htmlspecialchars(strip_tags($this->description));
         $this->categories_id=htmlspecialchars(strip_tags($this->categories_id));
-        $this->created_at=htmlspecialchars(strip_tags($this->created_at));
-
+    
         $query->bindParam(":nom", $this->nom);
         $query->bindParam(":prix", $this->prix);
         $query->bindParam(":description", $this->description);
         $query->bindParam(":categories_id", $this->categories_id);
-        $query->bindParam(":created_at", $this->created_at);
-
+    
         if($query->execute()){
             return true;
         }
@@ -71,27 +69,73 @@ class Produits{
      *
      * @return void
      */
-    function lireUn(){
-
-        // query to read single record
+    public function lireUn(){
         $sql = "SELECT c.nom as categories_nom, p.id, p.nom, p.description, p.prix, p.categories_id, p.created_at FROM " . $this->table . " p LEFT JOIN categories c ON p.categories_id = c.id WHERE p.id = ? LIMIT 0,1";
-
+    
         $query = $this->connexion->prepare( $sql );
-
-        // bind id of product to be updated
+    
         $query->bindParam(1, $this->id);
-
-        // execute query
+    
         $query->execute();
 
-        // get retrieved row
+        // on récupère la ligne
         $row = $query->fetch(PDO::FETCH_ASSOC);
-
-        // set values to object properties
+ 
+        // On hydrate l'objet
         $this->nom = $row['nom'];
         $this->prix = $row['prix'];
         $this->description = $row['description'];
         $this->categories_id = $row['categories_id'];
         $this->categories_nom = $row['categories_nom'];
+    }
+
+    /**
+     * Supprimer un produit
+     *
+     * @return void
+     */
+    public function supprimer(){
+        $sql = "DELETE FROM " . $this->table . " WHERE id = ?";
+    
+        $query = $this->connexion->prepare( $sql );
+    
+        $this->id=htmlspecialchars(strip_tags($this->id));
+
+        $query->bindParam(1, $this->id);
+    
+        if($query->execute()){
+            return true;
+        }
+     
+        return false;
+    }
+
+    /**
+     * Mettre à jour un produit
+     *
+     * @return void
+     */
+    public function modifier(){
+        $sql = "UPDATE " . $this->table . " SET nom = :nom, prix = :prix, description = :description, categories_id = :categories_id WHERE id = :id";
+     
+        $query = $this->connexion->prepare($sql);
+     
+        $this->nom=htmlspecialchars(strip_tags($this->nom));
+        $this->prix=htmlspecialchars(strip_tags($this->prix));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->categories_id=htmlspecialchars(strip_tags($this->categories_id));
+        $this->id=htmlspecialchars(strip_tags($this->id));
+     
+        $query->bindParam(':nom', $this->nom);
+        $query->bindParam(':prix', $this->prix);
+        $query->bindParam(':description', $this->description);
+        $query->bindParam(':categories_id', $this->categories_id);
+        $query->bindParam(':id', $this->id);
+     
+        if($query->execute()){
+            return true;
+        }
+     
+        return false;
     }
 }
